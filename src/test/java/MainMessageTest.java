491120798b92ccc9a2e154930cfdc5433b5143cb
Ujjwal Shrestha <ushrestha@zipcodes-MBP.fios-router.home>
2020-03-28 14:01:23 -0400

    few more test left

--- src/test/java/MainMessageTest.java
+++ src/test/java/MainMessageTest.java
@@ -46,5 +46,52 @@ public class MainMessageTest {
         Assert.assertTrue(m1.getReactions().containsValue(reactionEmoji));
     }
 
+    @Test
+    public void removeTest(){
+        Message m1 = new MainMessage();
+        String reactionName = "Happy";
+        String reactionEmoji = ":)";
+        m1.add(reactionName,reactionEmoji);
+        Assert.assertTrue(m1.getReactions().containsValue(reactionEmoji));
+        m1.remove(reactionName);
+        Assert.assertFalse(m1.getReactions().containsValue(reactionEmoji));
+    }
+
+    @Test
+    public void lookupTest(){
+        Message m1 = new MainMessage();
+        String expectedName = "Happy";
+        String expectedEmoji = ":)";
+        m1.add(expectedName,expectedEmoji);
+        Assert.assertTrue(m1.getReactions().containsValue(expectedEmoji));
+        String actualEmoji = m1.lookup(expectedName);
+        Assert.assertEquals(expectedEmoji,actualEmoji);
+    }
+
+//    @Test
+//    public void getSpeaker() {
+//        Message m1 = new MainMessage();
+////        m1.setSpeaker();
+//        User expected = m1.getSpeaker();
+//
+//        Assert.assertEquals(expected,m1.setSpeaker());
+//    }
+
+        @Test
+    public void getContent() {
+        Message m1 = new MainMessage();
+        m1.setContent("Hello World");
+        String expected = m1.getContent();
+        Assert.assertEquals(expected,"Hello World");
+    }
+
+    @Test
+    public void setContent() {
+        Message m1 = new MainMessage();
+        m1.setContent("Hello World");
+        Assert.assertTrue("Hello World",true);
+    }
+
+
 }
 
