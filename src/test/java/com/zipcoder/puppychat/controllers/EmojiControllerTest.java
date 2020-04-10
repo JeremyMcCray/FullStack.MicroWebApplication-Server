5e7f3368dc214bb09f8b26843625d31b305ed608
jc-hsiao <marshfire@live.com>
2020-04-10 01:42:32 -0400

    adding some tests for emoji controller

---
+++ src/test/java/com/zipcoder/puppychat/controllers/EmojiControllerTest.java
@@ -0,0 +1,61 @@
+package com.zipcoder.puppychat.controllers;
+
+import com.fasterxml.jackson.databind.util.ArrayIterator;
+import com.zipcoder.puppychat.error.NotFoundException;
+import com.zipcoder.puppychat.models.Emoji;
+import com.zipcoder.puppychat.services.EmojiService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EmojiControllerTest {
+
+    @Mock
+    EmojiService service;
+
+    @InjectMocks
+    EmojiController controller;
+
+    @Test
+    public void getEmoji() {
+        Mockito.when(service.findById(100)).thenReturn(new Emoji());
+        Assert.assertEquals( HttpStatus.OK, controller.getEmoji(100).getStatusCode());
+    }
+
+    @Test
+    public void getAllEmoji() {
+        Mockito.when(service.findAll()).thenReturn(new ArrayList<>());
+        Assert.assertEquals( HttpStatus.OK, controller.getAllEmoji().getStatusCode());
+    }
+
+    @Test
+    public void createEmoji() {
+        Emoji e = new Emoji();
+        Mockito.when(service.create(e)).thenReturn(e);
+        Assert.assertEquals( HttpStatus.OK, controller.createEmoji(e).getStatusCode());
+    }
+
+    @Test
+    public void updateEmoji() {
+        Emoji e = new Emoji();
+        Mockito.when(service.update(20,e)).thenReturn(e);
+        Assert.assertEquals( HttpStatus.OK, controller.updateEmoji(20,e).getStatusCode());
+    }
+
+    @Test
+    public void deleteEmoji() {
+        Mockito.when(service.delete(100)).thenReturn(new Emoji());
+        Assert.assertEquals( HttpStatus.OK, controller.deleteEmoji(100).getStatusCode());
+
+    }
+}
