f4ac0a729d1d327ae01678da13d1804fa323173c
JeremyMccray <jeremy.mccray1337@gmail.com>
2020-04-10 10:20:43 -0400

    more pass

--- src/test/java/com/zipcoder/puppychat/services/UserServiceTest.java
+++ src/test/java/com/zipcoder/puppychat/services/UserServiceTest.java
@@ -56,10 +56,31 @@ public class UserServiceTest {
 
     @Test
     public void changePassword() {
+        int uid = 1;
+        User u = new User();
+
+        u.setPassword("pass");
+
+        Mockito.when(repository.findById(uid)).thenReturn(Optional.of(u));
+        Mockito.when(repository.save(any())).thenReturn(u);
+
+       Assert.assertEquals(u,service.changePassword(1,"poss"));
+       Assert.assertEquals(u.getPassword(),"poss");
+
     }
 
     @Test
     public void changeEmail() {
+        int uid = 1;
+        User u = new User();
+
+        u.setEmail("tomsEmail@Email.com");
+
+        Mockito.when(repository.findById(uid)).thenReturn(Optional.of(u));
+        Mockito.when(repository.save(any())).thenReturn(u);
+
+        Assert.assertEquals(u,service.changeEmail(1,"poss"));
+        Assert.assertEquals(u.getEmail(),"poss");
     }
 
     @Test
